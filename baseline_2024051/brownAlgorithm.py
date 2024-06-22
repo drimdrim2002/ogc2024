cc6e4daeb330579498900b3bf2b6502c10cc9f17
drimdrim2002 <drimdrim2002@naver.com>
2024-06-22 17:29:54 +0900

    [brown] 일단 distance 적용

--- baseline_2024051/brownAlgorithm.py
+++ baseline_2024051/brownAlgorithm.py
@@ -2,7 +2,6 @@ import numpy as np
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 
-
 def algorithm(K, all_orders, all_riders, dist_mat, timelimit=60):
     print(f'K : {K}')
 
@@ -14,38 +13,121 @@ def algorithm(K, all_orders, all_riders, dist_mat, timelimit=60):
 
     data = make_input_data(K, dist_mat, all_orders, all_riders)
 
-    print(data)
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(
+        len(data["distance_matrix"]), data["num_vehicles"], data["depot"]
+    )
+
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+
+    # Create and register a transit callback.
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data["distance_matrix"][from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Add Distance constraint.
+    # dimension_name = "Distance"
+    # routing.AddDimension(
+    #     transit_callback_index,
+    #     0,  # no slack
+    #     99999,  # vehicle maximum travel distance
+    #     True,  # start cumul to zero
+    #     dimension_name,
+    # )
+    # distance_dimension = routing.GetDimensionOrDie(dimension_name)
+    # distance_dimension.SetGlobalSpanCostCoefficient(100)
+
+    # Define Transportation Requests.
+    # for request in data["pickups_deliveries"]:
+    #     pickup_index = manager.NodeToIndex(request[0])
+    #     delivery_index = manager.NodeToIndex(request[1])
+    #     routing.AddPickupAndDelivery(pickup_index, delivery_index)
+    #     routing.solver().Add(
+    #         routing.VehicleVar(pickup_index) == routing.VehicleVar(delivery_index)
+    #     )
+    #     routing.solver().Add(
+    #         distance_dimension.CumulVar(pickup_index)
+    #         <= distance_dimension.CumulVar(delivery_index)
+    #     )
+
+    # Add Capacity constraint.
+    # def demand_callback(from_index):
+    #     """Returns the demand of the node."""
+    #     # Convert from routing variable Index to demands NodeIndex.
+    #     from_node = manager.IndexToNode(from_index)
+    #     return data["demands"][from_node]
+    #
+    # demand_callback_index = routing.RegisterUnaryTransitCallback(demand_callback)
+    # routing.AddDimensionWithVehicleCapacity(
+    #     demand_callback_index,
+    #     0,  # null capacity slack
+    #     data["vehicle_capacities"],  # vehicle maximum capacities
+    #     True,  # start cumul to zero
+    #     "Capacity",
+    # )
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+    )
+    search_parameters.local_search_metaheuristic = (
+        routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH
+    )
+    search_parameters.time_limit.FromSeconds(1)
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+
+    # Print solution on console.
+    if solution:
+        print_solution(data, manager, routing, solution)
+
+
+
+    # print(data)
 
 
 def make_input_data(K, dist_mat, all_orders, all_riders):
     data = {}
-    data["depot"] = 0
 
-    data["distance_matrix"] = np.zeros((2 * K + 1, 2 * K + 1))
-    for row_index in range(K):
-        for column_index in range(K):
-            data["distance_matrix"][row_index + 1][column_index + 1] = dist_mat[row_index][column_index]
-    data["distance_matrix"] = data["distance_matrix"].tolist()
+    data["depot"] = 0 # dummy depot
 
+    data["distance_matrix"] = make_distance_matrix(K, dist_mat)
 
-    data["pickups_deliveries"] = np.zeros((K, 2))
-    for order_index in range(K):
-        data["pickups_deliveries"][order_index][0] = int(order_index + 1)
-        data["pickups_deliveries"][order_index][1] = int(order_index + 1 + K)
-    data["pickups_deliveries"] = data["pickups_deliveries"].tolist()
+    data["pickups_deliveries"] = make_pickup_delivery(K)
 
-    data["demands"] = []
-    data["demands"].append(0)
-    for order in all_orders:
-        data["demands"].append(int(order.volume))
+    data["demands"] = make_demand(K, all_orders)
 
-    data["vehicle_capacities"] = []
+    vehicle_capacity_arr = []
+    num_vehicles = 0
     for rider in all_riders:
-        for _ in len(rider.available_number):
-            data["vehicle_capacities"].append(rider.capa)
+        num_vehicles += rider.available_number
+        for _ in range(rider.available_number):
+            vehicle_capacity_arr.append(rider.capa)
 
+    data["num_vehicles"] = num_vehicles
+    data["vehicle_capacities"] = vehicle_capacity_arr
 
     return data
+
+
+def make_demand(K, all_orders):
+    demand_array = [0]
+    for _ in all_orders:
+        demand_array.append(0)
+    for order in all_orders:
+        demand_array.append(int(order.volume))
+    return demand_array
 
 
 def make_distance_matrix(K, dist_mat):
@@ -53,5 +135,41 @@ def make_distance_matrix(K, dist_mat):
     for row_index in range(K):
         for column_index in range(K):
             new_dist_matrix[row_index + 1][column_index + 1] = dist_mat[row_index][column_index]
-    new_dist_matrix = new_dist_matrix.tolist()
-    return new_dist_matrix
+
+    # set long distance from customer to shop
+    for customer_index in range(K + 1, 2 * K):
+        for shop_index in range(1, K):
+            new_dist_matrix[customer_index][shop_index] = 9999
+
+    tolist = new_dist_matrix.tolist()
+    return tolist
+
+
+def make_pickup_delivery(K):
+    np_array = np.zeros((K, 2))
+    for order_index in range(K):
+        np_array[order_index][0] = int(order_index + 1)
+        np_array[order_index][1] = int(order_index + 1 + K)
+
+    return np_array.astype(int).tolist()
+
+def print_solution(data, manager, routing, solution):
+    """Prints solution on console."""
+    print(f"Objective: {solution.ObjectiveValue()}")
+    max_route_distance = 0
+    for vehicle_id in range(data["num_vehicles"]):
+        index = routing.Start(vehicle_id)
+        plan_output = f"Route for vehicle {vehicle_id}:\n"
+        route_distance = 0
+        while not routing.IsEnd(index):
+            plan_output += f" {manager.IndexToNode(index)} -> "
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id
+            )
+        plan_output += f"{manager.IndexToNode(index)}\n"
+        plan_output += f"Distance of the route: {route_distance}m\n"
+        print(plan_output)
+        max_route_distance = max(route_distance, max_route_distance)
+    print(f"Maximum of the route distances: {max_route_distance}m")
